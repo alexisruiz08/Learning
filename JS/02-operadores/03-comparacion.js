3d6ae89060544aa9fe2c8b13d0dcd1755036c1a7
alexisruiz08 <alexisruizdiaz14@gmail.com>
2024-08-26 20:21:03 -0300

    Avance 8

---
+++ JS/02-operadores/03-comparacion.js
@@ -0,0 +1,17 @@
+let a = 10;
+
+// operadores relacionales
+
+console.log(a > 5); 
+console.log(a >= 5); 
+console.log(a < 5); 
+console.log(a <= 5);
+
+// operadores de igualdad
+
+console.log(a == 10); // igual
+console.log(a != 10); // distinto
+console.log(a === '10'); // compara valor y tipo de dato
+console.log(a !== 5); // distinto tipo de dato y de valor
+
+
---
+++ JS/02-operadores/04-logicos.js
@@ -0,0 +1,23 @@
+// AND, OR, NOT
+
+// AND &&
+
+let mayor = false;
+let suscrito = true;
+
+console.log('Operador AND', mayor && suscrito); 
+
+console.log(true && true); // obtiene valor true o false
+console.log(false && true);
+
+// OR ||
+
+console.log('Operador OR', mayor || suscrito);
+
+// NOT !
+
+console.log('Operador NOT', !mayor);
+let catalogoInfantil = !mayor;
+
+
+
